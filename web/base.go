9e6a2fcf00c80f0d0413fc17fe61e8a93dd1bab8
BryceWu <bryce.sun@outlook.com>
2020-04-29 00:26:44 +0800

    some change

--- web/base.go
+++ web/base.go
@@ -8,6 +8,11 @@ func init() {
 	router = httprouter.New()
 }
 
+type errorInfo struct {
+	code int
+	msg  string
+}
+
 func Httprouter() *httprouter.Router {
 	return router
 }
--- web/user.go
+++ web/user.go
@@ -8,14 +8,27 @@ import (
 
 func init() {
 	router := Httprouter()
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
+	router.PUT("/user/account", register)
+	router.PUT("/user/token", login)
+	router.DELETE("/user/token", logout)
 }
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+func register(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	if len(username) == 0 || len(password) == 0 {
+		w.WriteHeader(http.StatusNotAcceptable)
+		e := errorInfo{http.StatusNotAcceptable, "昵称密码不可为空"}
+		fmt.Fprint(w, e)
+	} else {
+		fmt.Fprint(w, "Welcome!\n")
+	}
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func login(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s! %v\n", ps.ByName("name"), r)
+}
+
+func logout(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s! %v\n", ps.ByName("name"), r)
 }
